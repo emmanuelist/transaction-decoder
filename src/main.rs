4862ad0d1e7c5b1fc6c6ca8320d25a75c002f02e
emmanuelist <emmanuel.paul75@yahoo.com>
2025-01-17 12:07:18 +0100

    Implement transaction hex validation and improve user input handling in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-fn  main() {
+fn main() {
     println!("Welecome to the Transaction Decoder!");
 
     println!("Enter a transaction hex: ");
@@ -9,5 +9,41 @@ fn  main() {
         .read_line(&mut transaction_hex)
         .expect("Failed to read line");
 
-    println!("You entered: {}", transaction_hex.trim());
+    let transaction_hex = transaction_hex.trim();
+
+    match validate_hex(transaction_hex) {
+        Ok(_) => {
+            println!("Valid transaction hex: {}", transaction_hex);
+            // Proceed with decoding logic
+        }
+        Err(err) => {
+            eprintln!("Error: {}", err);
+        }
+    }
+
+    // println!("You entered: {}", transaction_hex.trim());
+}
+
+/// Validate iif the input string is a valid hexadecimal string.
+///
+/// # Arguments
+/// * `hex` - A string slice that holds the hexadecimal string.
+///
+/// Returns
+///  * `Ok(())` if the input string is a valid hexadecimal string.
+/// * `Err(String)` if the input string is not a valid hexadecimal string.
+fn validate_hex(hex: &str) -> Result<(), String> {
+    if hex.is_empty() {
+        return Err("Empty string".to_string());
+    }
+
+    if hex.len() % 2 != 0 {
+        return Err("Hex string must have an even number of characters".to_string());
+    }
+
+    if !hex.chars().all(|c| c.is_ascii_hexdigit()) {
+        return Err("Hex string must contain only valid hexadecimal characters".to_string());
+    }
+
+    Ok(())
 }
